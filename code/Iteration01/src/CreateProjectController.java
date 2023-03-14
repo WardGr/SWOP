484b7ea856b78e4c113a83044ce9408e27425dec
Dietervh29 <dieter.vanhooydonk@student.kuleuven.be>
2023-03-14 21:34:51 +0100

    decoupling createProject UI en controller

--- code/Iteration01/src/CreateProjectController.java
+++ code/Iteration01/src/CreateProjectController.java
@@ -1,51 +1,32 @@
 public class CreateProjectController {
 
-  private Session session;
-  private TaskManSystem taskManSystem;
-  private CreateProjectUI createProjectUI;
+  private final Session session;
+  private final TaskManSystem taskManSystem;
 
   public CreateProjectController(
     Session session,
-    CreateProjectUI createProjectUI,
     TaskManSystem taskManSystem
   ) {
     this.session = session;
-    this.createProjectUI = createProjectUI;
     this.taskManSystem = taskManSystem;
   }
 
-  public void createProjectForm() {
-    if (session.getRole() != Role.PROJECTMANAGER) {
-      createProjectUI.printAccessError(Role.PROJECTMANAGER);
-      return;
-    }
-    createProjectUI.createProjectForm();
+  private Session getSession() {
+    return session;
   }
 
-  public void createProject(
-    String projectName,
-    String projectDescription,
-    int dueHour,
-    int dueMinute
-  ) {
-    if (session.getRole() != Role.PROJECTMANAGER) {
-      createProjectUI.printAccessError(Role.PROJECTMANAGER);
-      return;
+  private TaskManSystem getTaskManSystem() {
+    return taskManSystem;
+  }
+
+  public boolean createProjectPreconditions() {
+    return getSession().getRole() == Role.PROJECTMANAGER;
+  }
+
+  public void createProject(String projectName, String projectDescription, int dueHour, int dueMinute) throws IncorrectPermissionException, ProjectNameAlreadyInUseException, NotValidTimeException, DueBeforeSystemTimeException {
+    if (getSession().getRole() != Role.PROJECTMANAGER) {
+      throw new IncorrectPermissionException();
     }
-    try {
-      taskManSystem.createProject(
-        projectName,
-        projectDescription,
-        dueHour,
-        dueMinute
-      );
-      createProjectUI.messageProjectCreation(projectName);
-    } catch (NotValidTimeException e) {
-      createProjectUI.notValidTimeError();
-    } catch (DueBeforeSystemTimeException e) {
-      createProjectUI.dueBeforeSystemTimeError();
-    } catch (ProjectNameAlreadyInUseException e) {
-      createProjectUI.projectAlreadyInUseError();
-    }
+    getTaskManSystem().createProject(projectName, projectDescription, dueHour, dueMinute);
   }
 }
--- code/Iteration01/src/CreateProjectUI.java
+++ code/Iteration01/src/CreateProjectUI.java
@@ -2,107 +2,117 @@ import java.util.Scanner;
 
 public class CreateProjectUI {
 
-  private CreateProjectController createProjectController;
+  private final CreateProjectController controller;
 
   public CreateProjectUI(Session session, TaskManSystem taskManSystem) {
-    this.createProjectController =
-      new CreateProjectController(session, this, taskManSystem);
+    this.controller = new CreateProjectController(session, taskManSystem);
   }
 
   public void createProject() {
-    createProjectController.createProjectForm();
+    if (controller.createProjectPreconditions()){
+      createProjectForm();
+    } else {
+      permissionError();
+    }
   }
 
   public void createProjectForm() {
     Scanner scanner = new Scanner(System.in);
 
-    System.out.println("Type BACK to cancel project creation at any time");
-    System.out.println("*********** PROJECT CREATION FORM ***********");
+    while(true) {
 
-    System.out.print("Project Name: ");
-    String projectName = scanner.nextLine();
-    if (projectName.equals("BACK")) {
-      return;
-    }
+      System.out.println("Type BACK to cancel project creation at any time");
+      System.out.println("*********** PROJECT CREATION FORM ***********");
 
-    System.out.print("Project Description: ");
-    String projectDescription = scanner.nextLine();
-    if (projectDescription.equals("BACK")) {
-      return;
-    }
+      System.out.print("Project Name: ");
+      String projectName = scanner.nextLine();
+      if (projectName.equals("BACK")) {
+        return;
+      }
 
-    System.out.print("Project due hour: ");
-    String dueHourString = scanner.nextLine();
+      System.out.print("Project Description: ");
+      String projectDescription = scanner.nextLine();
+      if (projectDescription.equals("BACK")) {
+        return;
+      }
 
-    int dueHour;
-    while (true) {
-      try {
-        if (dueHourString.equals("BACK")) {
-          return;
+      System.out.print("Project due hour: ");
+      String dueHourString = scanner.nextLine();
+
+      int dueHour;
+      while (true) {
+        try {
+          if (dueHourString.equals("BACK")) {
+            return;
+          }
+          dueHour = Integer.parseInt(dueHourString);
+          break;
+        } catch (NumberFormatException e) {
+          System.out.println(
+                  "Given due hour is not an integer, please input an integer and try again"
+          );
+          dueHourString = scanner.nextLine();
         }
-        dueHour = Integer.parseInt(dueHourString);
-        break;
-      } catch (NumberFormatException e) {
-        System.out.println(
-          "Given due hour is not an integer, please input an integer and try again"
-        );
-        dueHourString = scanner.nextLine();
+      }
+
+      System.out.print("Project due minute: ");
+      String dueMinuteString = scanner.nextLine();
+
+      int dueMinute;
+      while (true) {
+        try {
+          if (dueMinuteString.equals("BACK")) {
+            return;
+          }
+          dueMinute = Integer.parseInt(dueMinuteString);
+          break;
+        } catch (NumberFormatException e) {
+          System.out.println(
+                  "Given due minute is not an integer, please input an integer and try again"
+          );
+          dueMinuteString = scanner.nextLine();
+        }
+      }
+
+      try {
+        controller.createProject(projectName, projectDescription, dueHour, dueMinute);
+        messageProjectCreation(projectName);
+        return;
+      } catch (ProjectNameAlreadyInUseException e) {
+        projectAlreadyInUseError();
+      } catch (NotValidTimeException e) {
+        notValidTimeError();
+      } catch (DueBeforeSystemTimeException e) {
+        dueBeforeSystemTimeError();
+      } catch (IncorrectPermissionException e) {
+        permissionError();
       }
     }
-
-    System.out.print("Project due minute: ");
-    String dueMinuteString = scanner.nextLine();
-
-    int dueMinute;
-    while (true) {
-      try {
-        if (dueMinuteString.equals("BACK")) {
-          return;
-        }
-        dueMinute = Integer.parseInt(dueMinuteString);
-        break;
-      } catch (NumberFormatException e) {
-        System.out.println(
-          "Given due minute is not an integer, please input an integer and try again"
-        );
-        dueMinuteString = scanner.nextLine();
-      }
-    }
-
-    createProjectController.createProject(
-      projectName,
-      projectDescription,
-      dueHour,
-      dueMinute
-    );
   }
 
-  public void messageProjectCreation(String projectName) {
+  private void messageProjectCreation(String projectName) {
     System.out.println("Project with name " + projectName + " created!");
   }
 
-  public void printAccessError(Role role) {
+  private void permissionError() {
     System.out.println(
       "You must be logged in with the " +
-      role.toString() +
+      Role.PROJECTMANAGER +
       " role to call this function"
     );
   }
 
   public void notValidTimeError() {
     System.out.println("The given time is not a valid time, please try again");
-    createProject();
   }
 
-  public void dueBeforeSystemTimeError() {
+  private void dueBeforeSystemTimeError() {
     System.out.println(
       "The given due time is before the current system time, please try again"
     );
-    createProject();
   }
 
-  public void projectAlreadyInUseError() {
+  private void projectAlreadyInUseError() {
     System.out.println("The project name is already in use");
-    createProject();
   }
 }
