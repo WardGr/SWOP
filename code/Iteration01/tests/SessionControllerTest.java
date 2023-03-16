4ff19b63328f9896decb7a6d53259db3efe73d32
WardGr <ward.grosemans@gmail.com>
2023-03-16 16:38:18 +0100

    Warnings sessiontest dinge weggehaald

--- code/Iteration01/tests/SessionControllerTest.java
+++ code/Iteration01/tests/SessionControllerTest.java
@@ -13,27 +13,17 @@ public class SessionControllerTest {
         assertFalse(sc.logout());
         assertTrue(sc.loginPrecondition());
         // enter wrong username / password
-        assertThrows( LoginException.class, () -> {
-            sc.login("Thomas", "1234");
-        });
-        assertThrows( LoginException.class, () -> {
-            sc.login("WardGr", "toilet573");
-        });
-        assertThrows( LoginException.class, () -> {
-            sc.login("OlavBl", "minecraft123");
-        });
+        assertThrows( LoginException.class, () -> sc.login("Thomas", "1234"));
+        assertThrows( LoginException.class, () -> sc.login("WardGr", "toilet573"));
+        assertThrows( LoginException.class, () -> sc.login("OlavBl", "minecraft123"));
         assertTrue(sc.loginPrecondition());
 
         sc.login("WardGr", "minecraft123");
         assertFalse(sc.loginPrecondition());
 
         //login while already logged in
-        assertThrows( LoginException.class, () -> {
-            sc.login("OlavBl", "toilet573");
-        });
-        assertThrows( LoginException.class, () -> {
-            sc.login("WardGr", "minecraft123");
-        });
+        assertThrows( LoginException.class, () -> sc.login("OlavBl", "toilet573"));
+        assertThrows( LoginException.class, () -> sc.login("WardGr", "minecraft123"));
         assertFalse(sc.loginPrecondition());
 
         assertTrue(sc.logout());
