104c0554f8c9f82e850187f092807f1cd39be2dc
Dietervh29 <dieter.vanhooydonk@student.kuleuven.be>
2023-05-22 23:40:30 +0200

    ProjectTest implemented

--- src/Tests/ProjectTest.java
+++ src/Tests/ProjectTest.java
@@ -8,8 +8,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -20,91 +22,163 @@ public class ProjectTest {
 
     @Mock
     private User user;
-    private Project project;
-    private List<Role> roles;
+    private Project project1;
+    private Project project2;
 
     @Before
-    public void setUp() throws InvalidTimeException, DueTimeBeforeCreationTimeException {
-        this.project = new Project("Project 1", "", new Time(0), new Time(100));
-        this.roles = List.of(Role.PYTHONPROGRAMMER);
+    public void setUp() throws InvalidTimeException, DueTimeBeforeCreationTimeException, TaskNameAlreadyInUseException, TaskNotFoundException, IllegalTaskRolesException, ProjectNotOngoingException, IncorrectTaskStatusException, LoopDependencyGraphException {
+        this.project1 = new Project("Project 1", "", new Time(0), new Time(100));
+        this.project2 = new Project("Project 2", "", new Time(0), new Time(100));
+        project1.addNewTask("Task1", "", new Time(0), 0, List.of(Role.SYSADMIN), new HashSet<>(), new HashSet<>());
+        project1.addNewTask("Task3", "", new Time(0), 0, List.of(Role.SYSADMIN), new HashSet<>(), new HashSet<>());
     }
 
     @Test
-    public void testProject() throws InvalidTimeException, TaskNameAlreadyInUseException, TaskNotFoundException, IncorrectTaskStatusException, LoopDependencyGraphException, IllegalTaskRolesException, ProjectNotOngoingException, EndTimeBeforeStartTimeException, IncorrectUserException, UserAlreadyAssignedToTaskException, IncorrectRoleException {
+    public void testCreationProject() throws InvalidTimeException, DueTimeBeforeCreationTimeException {
+        Project testProject = new Project("Project", "test", new Time(2), new Time(5));
+        assertEquals("Project", testProject.getName());
+        assertEquals("test", testProject.getDescription());
+        assertEquals(new Time(2), testProject.getCreationTime());
+        assertEquals(new Time(5), testProject.getDueTime());
 
-        /*
-        assertEquals("Project 1", project.getName());
-        assertEquals("", project.getDescription());
-        assertEquals(new Time(0), project.getCreationTime());
-        assertEquals(new Time(100), project.getDueTime());
-        assertEquals(ProjectStatus.ONGOING, project.getStatus());
-        assertEquals(List.of(), project.getTasks());
-        assertEquals(List.of(), project.getReplacedTasksNames());
-        assertEquals(List.of(), project.getActiveTasksNames());
-        assertNotNull(project.getProjectData());
+        assertEquals(0, testProject.getActiveTasksNames().size());
+        assertEquals(0, testProject.getReplacedTasksNames().size());
+        assertEquals(ProjectStatus.ONGOING, testProject.getStatus());
+        assertThrows(TaskNotFoundException.class, () -> testProject.getTaskData("Test"));
 
-        assertThrows(TaskNotFoundException.class, () -> project.getTaskData(""));
-        assertThrows(TaskNotFoundException.class, () -> project.getStatus(""));
-        assertThrows(TaskNotFoundException.class, () -> project.startTask("", new Time(0), user, Role.PROJECTMANAGER));
-        assertThrows(TaskNotFoundException.class, () -> project.finishTask("", user, new Time(0)));
-        assertThrows(TaskNotFoundException.class, () -> project.failTask("", user, new Time(0)));
-        assertThrows(TaskNotFoundException.class, () -> project.addNextTask("", ""));
-        assertThrows(TaskNotFoundException.class, () -> project.addPrevTask("", ""));
-        assertThrows(TaskNotFoundException.class, () -> project.removePrevTask("", ""));
-        assertThrows(TaskNotFoundException.class, () -> project.removeNextTask("", ""));
+        assertThrows(DueTimeBeforeCreationTimeException.class, () -> new Project("","", new Time(5), new Time(4)));
+    }
 
-        assertThrows(DueTimeBeforeCreationTimeException.class, () -> new Project("", "", new Time(10), new Time(0)));
+    @Test
+    public void testAddTask() throws InvalidTimeException, TaskNameAlreadyInUseException, TaskNotFoundException, IllegalTaskRolesException, ProjectNotOngoingException, IncorrectTaskStatusException, LoopDependencyGraphException {
+        project1.addNewTask("Task", "", new Time(5), 0.5, List.of(Role.SYSADMIN), new HashSet<>(), new HashSet<>());
+        assertTrue(project1.getActiveTasksNames().contains("Task"));
+
+        assertThrows(TaskNameAlreadyInUseException.class, () -> project1.addNewTask("Task", "", new Time(0), 0, List.of(Role.SYSADMIN), new HashSet<>(), new HashSet<>()));
+
+        assertThrows(TaskNotFoundException.class, () -> project1.deleteTask("Test"));
+        project1.deleteTask("Task");
+        assertFalse(project1.getActiveTasksNames().contains("Task"));
+    }
+
+    @Test
+    public void testDeleteTask() throws TaskNotFoundException {
+        assertThrows(TaskNotFoundException.class, () -> project1.deleteTask("test"));
+
+        project1.deleteTask("Task1");
+        assertFalse(project1.getActiveTasksNames().contains("Task1"));
+        assertEquals(0, project1.getReplacedTasksNames().size());
+    }
+
+    @Test
+    public void testCreationPreviousAndNextTasks() throws InvalidTimeException, TaskNameAlreadyInUseException, TaskNotFoundException, IllegalTaskRolesException, ProjectNotOngoingException, IncorrectTaskStatusException, LoopDependencyGraphException {
+        assertThrows(TaskNotFoundException.class, () -> project2.addNewTask("Task2", "", new Time(0), 0, List.of(Role.SYSADMIN), Set.of(new Tuple<>(project1, "Task")), new HashSet<>()));
+        assertThrows(TaskNotFoundException.class, () -> project2.addNewTask("Task2", "", new Time(0), 0, List.of(Role.SYSADMIN), new HashSet<>(), Set.of(new Tuple<>(project1, "Task"))));
+
+        project2.addNewTask("Task2", "", new Time(0), 0, List.of(Role.SYSADMIN), Set.of(new Tuple<>(project1, "Task1")), Set.of(new Tuple<>(project1, "Task3")));
+
+        assertTrue(project2.getTaskData("Task2").getPrevTaskNames().contains(new Tuple<>("Project 1", "Task1")));
+        assertTrue(project2.getTaskData("Task2").getNextTaskNames().contains(new Tuple<>("Project 1", "Task3")));
+    }
+
+    @Test
+    public void testAddingRemovingPreviousAndNextTasks() throws InvalidTimeException, TaskNameAlreadyInUseException, TaskNotFoundException, IllegalTaskRolesException, ProjectNotOngoingException, IncorrectTaskStatusException, LoopDependencyGraphException {
+        project2.addNewTask("Task2", "", new Time(0), 0, List.of(Role.SYSADMIN), Set.of(new Tuple<>(project1, "Task1")), Set.of(new Tuple<>(project1, "Task3")));
+
+        assertThrows(TaskNotFoundException.class, () -> project2.removePrevTask("Task", project1, "Task1"));
+        assertThrows(TaskNotFoundException.class, () -> project2.removeNextTask("Task", project1, "Task1"));
+        assertThrows(TaskNotFoundException.class, () -> project2.addPrevTask("Task", project1, "Task1"));
+        assertThrows(TaskNotFoundException.class, () -> project2.addNextTask("Task", project1, "Task1"));
+
+        project2.removePrevTask("Task2", project1, "Task1");
+        project2.removeNextTask("Task2", project1, "Task3");
+
+        assertEquals(0, project2.getTaskData("Task2").getPrevTaskNames().size());
+        assertEquals(0, project2.getTaskData("Task2").getNextTaskNames().size());
+
+        project2.addPrevTask("Task2", project1, "Task3");
+        project2.addNextTask("Task2", project1, "Task1");
+
+        assertTrue(project2.getTaskData("Task2").getPrevTaskNames().contains(new Tuple<>("Project 1", "Task3")));
+        assertTrue(project2.getTaskData("Task2").getNextTaskNames().contains(new Tuple<>("Project 1", "Task1")));
+    }
+
+    @Test
+    public void testStartTask() throws InvalidTimeException, TaskNotFoundException, IncorrectTaskStatusException, UserAlreadyAssignedToTaskException, IncorrectRoleException, IncorrectUserException {
+        assertThrows(TaskNotFoundException.class, () -> project1.startTask("Test", new Time(0), user, Role.SYSADMIN));
+        project1.startTask("Task1", new Time(0), user, Role.SYSADMIN);
+        assertEquals(Status.EXECUTING, project1.getTaskData("Task1").getStatus());
+
+        assertThrows(TaskNotFoundException.class, () -> project1.stopTask("Test", user));
+        project1.stopTask("Task1", user);
+        assertEquals(Status.AVAILABLE, project1.getTaskData("Task1").getStatus());
+    }
+
+    @Test
+    public void testFailFinishTask() throws InvalidTimeException, TaskNotFoundException, IncorrectTaskStatusException, UserAlreadyAssignedToTaskException, IncorrectRoleException, EndTimeBeforeStartTimeException, IncorrectUserException, TaskNameAlreadyInUseException, IllegalTaskRolesException, ProjectNotOngoingException, LoopDependencyGraphException {
+        project2.addNewTask("Task", "", new Time(0), 0, List.of(Role.SYSADMIN), new HashSet<>(), new HashSet<>());
+        project2.startTask("Task", new Time(5), user, Role.SYSADMIN);
+
+        assertThrows(TaskNotFoundException.class, () -> project2.failTask("Test", user, new Time(10)));
+        assertThrows(TaskNotFoundException.class, () -> project2.finishTask("Test", user, new Time(10)));
+
+        project2.failTask("Task", user, new Time(5));
+        assertEquals(Status.FAILED, project2.getTaskData("Task").getStatus());
+        assertEquals(ProjectStatus.ONGOING, project1.getStatus());
+
+        assertThrows(TaskNotFoundException.class, () -> project2.restartTask("Test"));
+        project2.restartTask("Task");
+        assertEquals(Status.EXECUTING, project2.getTaskData("Task").getStatus());
+        assertEquals(ProjectStatus.ONGOING, project2.getStatus());
+
+        project2.finishTask("Task", user, new Time(10));
+        assertEquals(Status.FINISHED, project2.getTaskData("Task").getStatus());
+        assertEquals(ProjectStatus.FINISHED, project2.getStatus());
+
+        project2.restartTask("Task");
+        assertEquals(Status.EXECUTING, project2.getTaskData("Task").getStatus());
+        assertEquals(ProjectStatus.ONGOING, project2.getStatus());
+    }
+
+    @Test
+    public void testReplaceTask() throws InvalidTimeException, TaskNotFoundException, IncorrectTaskStatusException, UserAlreadyAssignedToTaskException, IncorrectRoleException, EndTimeBeforeStartTimeException, IncorrectUserException, TaskNameAlreadyInUseException {
+        project1.startTask("Task1", new Time(0), user, Role.SYSADMIN);
+        project1.failTask("Task1", user, new Time(5));
+
+        assertThrows(TaskNameAlreadyInUseException.class, () -> project1.replaceTask("Task1", "", new Time(0), 0, "Task1"));
+        assertThrows(TaskNotFoundException.class, () -> project1.replaceTask("Replace", "", new Time(0), 0, "Test"));
+        project1.replaceTask("Replace", "", new Time(5), 0, "Task1");
 
 
-        project.addNewTask("Previous Task", "", new Time(10), 0, roles, Set.of(), Set.of());
+        assertTrue(project1.getReplacedTasksNames().contains("Task1"));
+        assertTrue(project1.getActiveTasksNames().contains("Replace"));
+        assertFalse(project1.getReplacedTasksNames().contains("Replace"));
+        assertFalse(project1.getActiveTasksNames().contains("Task1"));
+        assertEquals(ProjectStatus.ONGOING, project1.getStatus());
 
-        assertThrows(TaskNameAlreadyInUseException.class, () -> project.addNewTask("Previous Task", "", new Time(10), 0, roles, Set.of(), Set.of()));
-        assertThrows(TaskNotFoundException.class, () -> project.addNewTask("Task", "", new Time(10), 0, roles, Set.of(""), Set.of()));
-        assertThrows(TaskNotFoundException.class, () -> project.addNewTask("Task", "", new Time(10), 0, roles, Set.of(), Set.of("")));
+        project1.deleteTask("Task3");
+        project1.startTask("Replace", new Time(10), user, Role.SYSADMIN);
+        project1.finishTask("Replace", user, new Time(15));
+        assertEquals(ProjectStatus.FINISHED, project1.getStatus());
 
-        project.addNewTask("Next Task", "", new Time(0), 0, roles, Set.of(), Set.of());
-        project.addNewTask("Current Task", "", new Time(10), 0, roles, Set.of("Previous Task"), Set.of("Next Task"));
+        assertThrows(ProjectNotOngoingException.class, () -> project1.addNewTask("","", new Time(0), 0, List.of(Role.SYSADMIN), new HashSet<>(), new HashSet<>()));
+    }
 
-        assertEquals("ongoing", project.getStatus().toString());
+    @Test
+    public void testClearTasks() throws InvalidTimeException, TaskNotFoundException, IncorrectTaskStatusException, UserAlreadyAssignedToTaskException, IncorrectRoleException, EndTimeBeforeStartTimeException, IncorrectUserException, TaskNameAlreadyInUseException {
+        project1.startTask("Task1", new Time(0), user, Role.SYSADMIN);
+        project1.failTask("Task1", user, new Time(5));
+        project1.replaceTask("Replace", "", new Time(5), 0, "Task1");
+        project1.startTask("Replace", new Time(10), user, Role.SYSADMIN);
+        project1.finishTask("Replace", user, new Time(15));
+        project1.startTask("Task3", new Time(20), user, Role.SYSADMIN);
+        project1.finishTask("Task3", user, new Time(25));
+        assertEquals(ProjectStatus.FINISHED, project1.getStatus());
 
-        assertNotNull(project.getStatus("Previous Task"));
-
-        project.startTask("Previous Task", new Time(0), user, Role.PYTHONPROGRAMMER);
-        assertEquals(Status.EXECUTING, project.getStatus("Previous Task"));
-        project.finishTask("Previous Task", user, new Time(20));
-        assertEquals(Status.FINISHED, project.getStatus("Previous Task"));
-        assertEquals(Status.AVAILABLE, project.getStatus("Current Task"));
-
-        project.startTask("Current Task", new Time(20), user, Role.PYTHONPROGRAMMER);
-        assertEquals(Status.EXECUTING, project.getStatus("Current Task"));
-        project.finishTask("Current Task", user, new Time(40));
-        assertEquals(Status.FINISHED, project.getStatus("Current Task"));
-        assertEquals(Status.AVAILABLE, project.getStatus("Next Task"));
-
-        project.startTask("Next Task", new Time(40), user, Role.PYTHONPROGRAMMER);
-        assertEquals(Status.EXECUTING, project.getStatus("Next Task"));
-        project.finishTask("Next Task", user, new Time(60));
-        assertEquals(Status.FINISHED, project.getStatus("Next Task"));
-
-        assertEquals("finished", project.getStatus().toString());
-
-        assertThrows(ProjectNotOngoingException.class, () -> project.addNewTask("", "", new Time(10), 0, List.of(), Set.of(), Set.of()));
-
-        // PROJECT PROXY TEST
-
-        ProjectData projectData = project.getProjectData();
-
-        assertEquals(project.getName(), projectData.getName());
-        assertEquals(project.getStatus(), projectData.getStatus());
-        assertEquals(project.getReplacedTasksNames(), projectData.getReplacedTasksNames());
-        assertEquals(project.getActiveTasksNames(), projectData.getActiveTasksNames());
-        assertEquals(project.getReplacedTasksNames(), projectData.getReplacedTasksNames());
-        assertEquals(project.getCreationTime(), projectData.getCreationTime());
-        assertEquals(project.getDueTime(), projectData.getDueTime());
-        assertEquals(project.getDescription(), projectData.getDescription());
-
-         */
-
+        project1.clearTasks();
+        assertEquals(0, project1.getActiveTasksNames().size());
+        assertEquals(0, project1.getReplacedTasksNames().size());
+        assertEquals(ProjectStatus.ONGOING, project1.getStatus());
 
     }
 }
