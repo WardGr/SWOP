b834c8eea820422be4a2ec4cea71b5838447e718
Olav Blaak <olav.blaak@student.kuleuven.be>
2023-05-27 16:18:00 +0200

    DeleteProjectControllerTest unit test

---
+++ src/Tests/ControllerTests/DeleteProjectControllerTest.java
@@ -0,0 +1,89 @@
+package Tests.ControllerTests;
+
+import Application.Command.CommandManager;
+import Application.IncorrectPermissionException;
+import Application.ProjectControllers.DeleteProjectController;
+import Application.Session.Session;
+import Application.Session.SessionProxy;
+import Application.TaskControllers.DeleteTaskController;
+import Domain.Project.ProjectData;
+import Domain.Task.Task;
+import Domain.TaskManSystem.TaskManSystem;
+import Domain.User.Role;
+import Domain.User.User;
+import Domain.DataClasses.Time;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+public class DeleteProjectControllerTest {
+
+    private Session session;
+    private User projectManager;
+    private User incorrect;
+    private TaskManSystem taskManSystem;
+    private DeleteProjectController deleteProjectController;
+
+    @Before
+    public void setUp() throws Exception {
+        this.session = new Session();
+        this.projectManager = new User("DieterVH", "computer776", Set.of(Role.PROJECTMANAGER));
+        this.incorrect = new User("OlavBl", "peer123", Set.of(Role.PYTHONPROGRAMMER));
+        this.taskManSystem = new TaskManSystem(new Time(0));
+        this.deleteProjectController = new DeleteProjectController(new SessionProxy(session), taskManSystem, new CommandManager());
+        session.login(projectManager);
+
+        taskManSystem.createProject("Omer", "Brew omer beer", new Time(30));
+        taskManSystem.createProject("Duvel", "Brew duvel beer", new Time(30));
+    }
+
+    @Test
+    public void testPreconditions() {
+        assertTrue(deleteProjectController.deleteProjectPreconditions());
+        session.logout();
+        assertFalse(deleteProjectController.deleteProjectPreconditions());
+        session.login(incorrect);
+        assertFalse(deleteProjectController.deleteProjectPreconditions());
+    }
+
+    @Test
+    public void testIncorrectPermissions() {
+        session.logout();
+        assertFalse(deleteProjectController.deleteProjectPreconditions());
+        assertThrows(IncorrectPermissionException.class, () -> deleteProjectController.deleteProject("Omer"));
+        assertThrows(IncorrectPermissionException.class, () -> deleteProjectController.getTaskManSystemData());
+        assertThrows(IncorrectPermissionException.class, () -> deleteProjectController.getProjectData("Omer"));
+
+        session.login(incorrect);
+        assertFalse(deleteProjectController.deleteProjectPreconditions());
+        assertThrows(IncorrectPermissionException.class, () -> deleteProjectController.deleteProject("Omer"));
+        assertThrows(IncorrectPermissionException.class, () -> deleteProjectController.getTaskManSystemData());
+        assertThrows(IncorrectPermissionException.class, () -> deleteProjectController.getProjectData("Omer"));
+    }
+
+    @Test
+    public void testGetters() throws Exception {
+        assertTrue(deleteProjectController.getTaskManSystemData().getProjectsData().stream().map(ProjectData::getName).toList().contains("Omer"));
+        assertEquals("Omer", deleteProjectController.getProjectData("Omer").getName());
+
+        assertTrue(deleteProjectController.getTaskManSystemData().getProjectsData().stream().map(ProjectData::getName).toList().contains("Duvel"));
+        assertEquals("Duvel", deleteProjectController.getProjectData("Duvel").getName());
+
+        assertEquals(2, deleteProjectController.getTaskManSystemData().getProjectsData().size());
+    }
+
+    @Test
+    public void testDeleteProject() throws Exception {
+        deleteProjectController.deleteProject("Omer");
+        assertFalse(deleteProjectController.getTaskManSystemData().getProjectsData().stream().map(ProjectData::getName).toList().contains("Omer"));
+        assertEquals(1, deleteProjectController.getTaskManSystemData().getProjectsData().size());
+
+        deleteProjectController.deleteProject("Duvel");
+        assertFalse(deleteProjectController.getTaskManSystemData().getProjectsData().stream().map(ProjectData::getName).toList().contains("Duvel"));
+        assertEquals(0, deleteProjectController.getTaskManSystemData().getProjectsData().size());
+    }
+}
---
+++ src/Tests/ControllerTests/DeleteTaskControllerTest.java
@@ -0,0 +1,4 @@
+package Tests.ControllerTests;
+
+public class DeleteTaskControllerTest {
+}
